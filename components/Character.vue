f21bfb1fb6190815010cd908041bb5cfa7694c96
Jorge <jghinestrosa@gmail.com>
2021-12-31 21:04:58 +0100

    Play with CSS grid to improve window in mobile

--- components/Character.vue
+++ components/Character.vue
@@ -2,9 +2,9 @@
 	<div class="character">
 		<h5>{{character.inSeinfeld.name}} is {{character.inMovie.name}}</h5>
 		<div class="character-data">
-			<CharacterImg :imgSrc="character.inMovie.img"/>
-			<p>{{character.description}}</p>
-			<CharacterImg :imgSrc="character.inSeinfeld.img"/>
+			<CharacterImg id="character-in-movie" :imgSrc="character.inMovie.img"/>
+			<p class="character-description">{{character.description}}</p>
+			<CharacterImg id="character-in-seinfeld" :imgSrc="character.inSeinfeld.img"/>
 		</div>
 	</div>
 </template>
@@ -30,10 +30,26 @@ export default {
 
 .character-data {
 	display: grid;
-	grid-template-columns: auto;
+	grid-template-columns: repeat(2, 1fr);
+	grid-template-rows: 1fr;
 	grid-gap: 10px;
 	justify-content: center;
 	margin-bottom: 20px;
+}
+
+#character-in-movie {
+	grid-column: 1;
+	grid-row: 1;
+}
+
+#character-in-seinfeld {
+	grid-column: 2;
+	grid-row: 1;
+}
+
+.character-description {
+	grid-column: 1 / 3;
+	grid-row: 2;
 }
 
 .character-data p {
@@ -43,8 +59,22 @@ export default {
 
 @media (min-width: 800px) {
 	.character-data {
-		grid-template-columns: 250px auto 250px;
-		justify-content: space-between;
+		/*grid-template-columns: 250px auto 250px;*/
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: 1fr;
+	}
+
+	#character-in-movie {
+		grid-column: 1;
+	}
+
+	#character-in-seinfeld {
+		grid-column: 3;
+	}
+
+	.character-description {
+		grid-column: 2;
+		grid-row: 1;
 	}
 
 	.character-data p {
